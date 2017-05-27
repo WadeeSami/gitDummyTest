5c22d076af02263f568a6d2c1d230a03294a06ba
Wadee Sami <wabuzant@endeavorpal.com>
2017-05-27 10:54:59 +0300

    changing methods in this features for educatuonal purposes only

--- a.py
+++ a.py
@@ -1,11 +1,11 @@
 def masterFunction():
-    print "This is the master function"
-
+    # print "This is the master function"
+    print "This is in feature 1"
 
 
 def main():
-    print "This is the master version of the main function"
-
+    # print "This is the master version of the main function"
+    print "This main function is modified in feature 1"
 
 main()
 
